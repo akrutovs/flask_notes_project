7ca79ba2bc280e10ee0158e66ed7d3aaac4d27a1
Aleksey <a.krutovs@ya.ru>
2022-05-14 08:51:41 +0300

    add user page

--- app.py
+++ app.py
@@ -4,9 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+@app.route('/home')
+def home():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/about')
+def about():
+    return 'its about page'
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name,id):
+    string = name + str(id)
+    return 'hi, '+string
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
