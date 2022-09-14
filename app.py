738a16a4646e879005966e44c22d6ad8162ba335
Alexey Krutov <a.krutovs@yandex.ru>
2022-09-14 15:27:07 +0300

    fix bug with password

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     registration_date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
